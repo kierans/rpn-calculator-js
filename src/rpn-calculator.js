7077561b3215ddd7313dc2ad3692d6ebad09d39b
Kieran Simpson <kierans777@gmail.com>
2025-09-30 19:56:07 +1000

    Add entry point for app.

--- src/rpn-calculator.js
+++ src/rpn-calculator.js
@@ -1,3 +1,38 @@
 "use strict";
 
-console.log("Hello World");
+const process = require("node:process");
+
+const IO = require("crocks/IO");
+
+const compose = require("crocks/helpers/compose");
+const constant = require("crocks/combinators/constant");
+const curry = require("crocks/helpers/curry");
+const fst = require("crocks/Pair/fst");
+const read = require("crocks/Writer/read");
+const snd = require("crocks/Pair/snd");
+const substitution = require("crocks/combinators/substitution");
+
+const { calculate } = require("./calculator");
+const { nextLine } = require("./scanner");
+const { printlns } = require("./printer");
+
+// log :: (List String -> Unit) -> Writer (List String) a -> a
+const log = (output) =>
+	compose(substitution(compose(constant, snd), compose(output, fst)), read)
+
+// readLoop :: (() -> Async Error String) -> (List String -> Unit) -> [Operation] -> Unit
+const readLoop = curry((input, output, stack) =>
+	input()
+		.map(compose(log(output), calculate(stack)))
+		.fork(
+			// unrecoverable error
+			(e) => { throw e },
+			readLoop(input)(output)
+		)
+)
+
+// main :: (Readable, Writable) -> IO Unit
+const main = (input, output) =>
+	IO(() => readLoop(nextLine(input))(printlns(output))([]))
+
+main(process.stdin, process.stdout).run();
